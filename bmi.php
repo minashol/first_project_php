469f6ef04e9b85396187c668f3617bd363c89dcf
minashol <minasholevas@gmail.com>
2022-09-14 18:34:51 +0300

    Added html forms and a crude way to calculate bmi

--- bmi.php
+++ bmi.php
@@ -12,15 +12,48 @@
 
       include "header.html";
 
+      ?>
 
 
 
+      <form action="bmi.php" method="post">
+
+        First Name: <input type="text" name="first_name"> <br>
+        Height (in meters): <input type="number" step=0.01 name="height"> <br>
+        Weight (in kilograms): <input type="number" step=0.1 name="kilos"> <br>
+
+        <input type="submit">
+
+
+      </form>
+
+
+
+
+
+
+
+      <?php
+
+      $name = $_POST["first_name"];
+      $height = $_POST["height"];
+      $weight = $_POST["kilos"];
+
+      $bmi = $weight / ($height * $height);
+
+      echo $bmi;
+
+
+       ?>
+
+
+      <?php
       include "footer.html";
       ?>
 
 
 
-      
+
 
 
   </body>

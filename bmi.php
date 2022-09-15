72c386e6007a842479d846dabf08b45d8c2fa4bc
minashol <minasholevas@gmail.com>
2022-09-15 19:30:56 +0300

    Added a link about BMI

--- bmi.php
+++ bmi.php
@@ -40,20 +40,22 @@
       $weight = $_POST["kilos"];
 
       $bmi = $weight / ($height * $height);
-
+      $bmi=round($bmi, 2);
       if ($bmi <=18.5){
-        echo "$name, you are underweight! You should gain some weight!";
+        echo "$name, you are underweight! You should gain some weight! Your BMI is $bmi";
 
       }elseif ($bmi<=24.9){
-        echo "$name, you are in the healthy weight range.";
+        echo "$name, you are in the healthy weight range. Your BMI is $bmi";
       }elseif($bmi<=29.9){
-        echo "$name, you are in the overweight range. Do some diet";
+        echo "$name, you are in the overweight range. Do some diet. Your BMI is $bmi";
       }else{
-        echo "$name, you are in the obese range! You should watch out your diet!";
+        echo "$name, you are in the obese range! You should watch out your diet! Your BMI is $bmi";
       }
 
 
        ?>
+       <br>
+       <a href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html" target="_blank">Click here</a> for more information about BMI.<br>
 
 
       <?php
